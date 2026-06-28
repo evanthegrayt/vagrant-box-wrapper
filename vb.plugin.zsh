e5f86179f39174bd33002d2b5e38ca37dd2a95dc
Evan Gray <evanthegrayt@vivaldi.net>
2026-06-28 11:59:17 -0500

    Refactor version 2.0
    - Fix tab-completion
    - Allow boxes to be in different directories
    - Remove .vbrc dependency in favor of startup file
    - Make VB_BOXES export-able

--- vb.plugin.zsh
+++ vb.plugin.zsh
@@ -1,8 +1,15 @@
 source "${0:h}/vb.sh"
 
 _vb() {
-    _arguments "1: :($( __vb_vagrant_args ))"
+    local -a matches
+
+    if [[ $CURRENT -eq 3 && ${words[2]} == use ]]; then
+        matches=("${(@f)$(__vb_box_names)}")
+    else
+        matches=("${(@f)$(__vb_vagrant_args)}")
+    fi
+
+    compadd -- "$matches[@]"
 }
 
 compdef '_vb' vb
-
--- vb.sh
+++ vb.sh
@@ -1,75 +1,196 @@
-vb() {
-    local b
-    local box
-    local index
-    local desired_box
+__vb_box_paths() {
+    local box_path
+    local paths="${VB_BOXES:-}"
+
+    while [[ -n $paths ]]; do
+        if [[ $paths == *:* ]]; then
+            box_path="${paths%%:*}"
+            paths="${paths#*:}"
+        else
+            box_path="$paths"
+            paths=''
+        fi
+
+        [[ -n $box_path ]] && printf '%s\n' "$box_path"
+    done
+}
+
+__vb_box_name() {
+    local box_path="${1%/}"
+
+    printf '%s\n' "${box_path##*/}"
+}
+
+__vb_box_names() {
+    local box_path
+
+    __vb_box_paths | while IFS= read -r box_path; do
+        __vb_box_name "$box_path"
+    done
+}
+
+__vb_first_box_name() {
+    local box_path
+
+    while IFS= read -r box_path; do
+        __vb_box_name "$box_path"
+        return 0
+    done < <(__vb_box_paths)
+
+    return 1
+}
+
+__vb_duplicate_box_name() {
+    local name
+    local newline=$'\n'
+    local seen="$newline"
+
+    while IFS= read -r name; do
+        if [[ $seen == *"${newline}${name}${newline}"* ]]; then
+            printf '%s\n' "$name"
+            return 0
+        fi
+
+        seen="${seen}${name}${newline}"
+    done < <(__vb_box_names)
+
+    return 1
+}
+
+__vb_box_path_for_name() {
+    local desired_name="$1"
+    local box_path
+    local name
+    local match=''
+    local matches=0
+
+    while IFS= read -r box_path; do
+        name="$(__vb_box_name "$box_path")"
+
+        if [[ $name == "$desired_name" ]]; then
+            match="$box_path"
+            (( matches++ ))
+        fi
+    done < <(__vb_box_paths)
+
+    if (( matches == 1 )); then
+        printf '%s\n' "$match"
+        return 0
+    elif (( matches > 1 )); then
+        return 2
+    fi
+
+    return 1
+}
+
+__vb_require_config() {
+    local duplicate
+
+    if [[ -z ${VB_BOXES:-} ]]; then
+        printf "${VB_ERROR_COLOR}To use this command, set " >&2
+        printf "\`export VB_BOXES=\"/path/to/box1:/path/to/box2\"' " >&2
+        printf "in your shell init file.$VB_RESET\n" >&2
+        return 1
+    fi
+
+    if ! __vb_first_box_name >/dev/null; then
+        printf "${VB_ERROR_COLOR}VB_BOXES does not contain any box paths." >&2
+        printf "$VB_RESET\n" >&2
+        return 1
+    fi
+
+    if duplicate="$(__vb_duplicate_box_name)"; then
+        printf "${VB_ERROR_COLOR}Duplicate box name [$duplicate] in " >&2
+        printf "\$VB_BOXES. Box directory names must be unique.$VB_RESET\n" >&2
+        return 1
+    fi
+}
+
+__vb_current_box() {
     local current_box
+    local first_box
+
+    if [[ -f $VB_CACHE ]]; then
+        current_box="$(< "$VB_CACHE")"
+
+        if __vb_box_path_for_name "$current_box" >/dev/null; then
+            printf '%s\n' "$current_box"
+            return 0
+        fi
+
+        printf "${VB_WARNING_COLOR}Cached box [$current_box] is not in " >&2
+        printf "\$VB_BOXES. " >&2
+    else
+        printf "${VB_WARNING_COLOR}Box not previously set. " >&2
+    fi
+
+    first_box="$(__vb_first_box_name)" || return 1
+    printf "Setting to first configured box.\n" >&2
+    printf "Use \`vb switch' to change.$VB_RESET\n" >&2
+    printf '%s\n' "$first_box" > "$VB_CACHE"
+    printf '%s\n' "$first_box"
+}
+
+vb() {
+    local box
+    local current_box
+    local desired_box
+    local next_box
+    local box_path
+    local seen_current=false
+    local working_dir
 
     local cmd="$1"
     local subcmd="$2"
 
-    if [[ -f $VB_CACHE ]]; then
-        current_box="$( < $VB_CACHE)"
-    else
-        printf "${VB_WARNING_COLOR}Box not previously set. "
-        printf "Setting to first box in array.\n"
-        printf "Use \`vb switch' to change.$VB_RESET\n"
-        echo ${VB_BOXES[1]} > $VB_CACHE
-        current_box="${VB_BOXES[1]}"
-    fi
+    __vb_require_config || return 1
 
-    if [[ -z $VB_BOXES ]]; then
-        printf "${VB_ERROR_COLOR}To use this command, you must set " >&2
-        printf "\`VB_BOXES=()' in a startup file, or \`~/.vbrc'$VB_RESET\n" >&2
-        return 1
-    elif [[ -z $VB_BOXES_LOCATION ]]; then
-        printf "${VB_ERROR_COLOR}To use this command, you must set " >&2
-        printf "\`VB_BOXES_LOCATION=\"\"' in a startup file, or " >&2
-        printf "\`~/.vbrc'$VB_RESET\n" >&2
-        return 1
-    fi
-
-    local working_dir="$VB_BOXES_LOCATION/$current_box"
+    current_box="$(__vb_current_box)" || return 1
+    working_dir="$(__vb_box_path_for_name "$current_box")" || return 1
 
     case "$cmd" in
         list)
-            for box in "${VB_BOXES[@]}"; do
-                if [[ $box == $current_box ]]; then
-                    printf "* ${VB_SUCCESS_COLOR}$box$VB_RESET\n"
+            while IFS= read -r box_path; do
+                box="$(__vb_box_name "$box_path")"
+
+                if [[ $box == "$current_box" ]]; then
+                    printf "* ${VB_SUCCESS_COLOR}%s$VB_RESET\n" "$box"
                 else
-                    echo "  $box"
+                    printf "  %s\n" "$box"
                 fi
-            done
+            done < <(__vb_box_paths)
             ;;
 
         switch)
-            index=1
+            while IFS= read -r box_path; do
+                box="$(__vb_box_name "$box_path")"
 
-            for b in "${VB_BOXES[@]}"; do
-                if [[ $b == $current_box ]]; then
-                    if (( $index == ${#VB_BOXES[@]} )); then
-                        box=${VB_BOXES[1]}
-                        break
-                    else
-                        box="${VB_BOXES[$(( index + 1 ))]}"
-                        break
-                    fi
+                if $seen_current; then
+                    next_box="$box"
+                    break
                 fi
-                (( index++ ))
-            done
 
-            echo $box > $VB_CACHE
-            printf "Switching to box [${VB_SUCCESS_COLOR}$box$VB_RESET]\n"
+                if [[ $box == "$current_box" ]]; then
+                    seen_current=true
+                fi
+            done < <(__vb_box_paths)
+
+            if [[ -z $next_box ]]; then
+                next_box="$(__vb_first_box_name)" || return 1
+            fi
+
+            printf '%s\n' "$next_box" > "$VB_CACHE"
+            printf "Switching to box [${VB_SUCCESS_COLOR}%s$VB_RESET]\n" "$next_box"
             ;;
 
         cd)
-            cd $working_dir
+            cd "$working_dir" || return 1
 
             if [[ -n $subcmd ]]; then
                 if [[ -d $subcmd ]]; then
-                    cd $subcmd
+                    cd "$subcmd" || return 1
                 else
-                    printf "Subdir [$VB_ERROR_COLOR$subcmd$VB_RESET] " >&2
+                    printf "Subdir [$VB_ERROR_COLOR%s$VB_RESET] " "$subcmd" >&2
                     printf "doesn't exist.\n" >&2
                     return 1
                 fi
@@ -77,39 +198,44 @@ vb() {
             ;;
 
         echo)
-            echo $working_dir
+            printf '%s\n' "$working_dir"
             ;;
 
         use)
             box="$subcmd"
 
             if [[ -z $box ]]; then
-                printf "${VB_ERROR_COLOR}No box specified.$VB_RESET\n"
+                printf "${VB_ERROR_COLOR}No box specified.$VB_RESET\n" >&2
                 return 1
             fi
 
-            for b in "${VB_BOXES[@]}"; do
-                if [[ $b == $box ]]; then
-                    desired_box="$box"
-                    break
-                fi
-            done
+            desired_box="$(__vb_box_path_for_name "$box")"
+            case $? in
+                0)
+                    ;;
+                2)
+                    printf "${VB_ERROR_COLOR}[$box] matches more than one " >&2
+                    printf "path in \$VB_BOXES.$VB_RESET\n" >&2
+                    return 1
+                    ;;
+                *)
+                    printf "${VB_ERROR_COLOR}[$box] " >&2
+                    printf "not in \$VB_BOXES$VB_RESET\n" >&2
+                    return 1
+                    ;;
+            esac
 
-            if [[ -z $desired_box ]]; then
-                printf "${VB_ERROR_COLOR}[$box] " >&2
-                printf "not in \$VB_BOXES$VB_RESET\n" >&2
-                return 1
-            fi
+            desired_box="$(__vb_box_name "$desired_box")"
 
-            if [[ $desired_box == $current_box ]]; then
-                printf "${VB_WARNING_COLOR}[$desired_box] "
+            if [[ $desired_box == "$current_box" ]]; then
+                printf "${VB_WARNING_COLOR}[%s] " "$desired_box"
                 printf "is already the current box.$VB_RESET\n"
                 return 0
             fi
 
-            echo $desired_box > $VB_CACHE
+            printf '%s\n' "$desired_box" > "$VB_CACHE"
             printf "Switching to box "
-            printf "[${VB_SUCCESS_COLOR}$desired_box$VB_RESET]\n"
+            printf "[${VB_SUCCESS_COLOR}%s$VB_RESET]\n" "$desired_box"
             ;;
 
         -h|'')
@@ -133,22 +259,28 @@ vb() {
             ;;
 
         *)
-            [[ $PWD == $working_dir ]] || cd "$working_dir"
-            vagrant "$@"
-            [[ $PWD == $OLDPWD ]] || cd $OLDPWD
-            ;;
+            local oldpwd="$PWD"
+            local vagrant_status
 
+            cd "$working_dir" || return 1
+            vagrant "$@"
+            vagrant_status=$?
+            cd "$oldpwd" || return 1
+
+            return "$vagrant_status"
+            ;;
     esac
 }
 
 __vb_vagrant_args() {
     printf "list\nuse\nswitch\ncd\necho\n"
-    vagrant -h | "grep" -E "^\s+" | awk '{print $1}' | tr -d ','
+
+    if command -v vagrant >/dev/null 2>&1; then
+        vagrant -h | grep -E "^[[:space:]]+" | awk '{print $1}' | tr -d ','
+    fi
 }
 
-[[ -f $HOME/.vbrc ]] && source $HOME/.vbrc
-
-if [[ -n $VB_CACHE ]]; then
+if [[ -n ${VB_CACHE:-} ]]; then
     if [[ -d $VB_CACHE || $VB_CACHE == */ ]]; then
         VB_CACHE=$VB_CACHE/vb.cache
     fi
@@ -156,7 +288,11 @@ else
     VB_CACHE="${ZSH_CACHE_DIR:-"$HOME/.cache/vb"}/vb.cache"
 fi
 
-[[ -d ${VB_CACHE%/*} ]] || mkdir -p ${VB_CACHE%/*}
+__vb_cache_dir="${VB_CACHE%/*}"
+if [[ $__vb_cache_dir != "$VB_CACHE" ]]; then
+    [[ -d $__vb_cache_dir ]] || mkdir -p "$__vb_cache_dir"
+fi
+unset __vb_cache_dir
 
 if ${VB_COLOR:=true}; then
     : ${VB_ERROR_COLOR:='\e[0;91m'}
@@ -166,4 +302,3 @@ if ${VB_COLOR:=true}; then
 else
     unset VB_ERROR_COLOR VB_SUCCESS_COLOR VB_WARNING_COLOR VB_RESET
 fi
-
